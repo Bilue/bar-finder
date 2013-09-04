3e8a55435f5e7e6b3f02add838dc11a6785c2b3f
Roberto Amici <roberto.amici@Development-1s-MacBook-Pro.local>
2013-09-04 18:44:59 +1000

    Adds nib for add bar viewcontroller

---
+++ Mi9 Bar Finder/View Controllers/Mi9AddBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  Mi9AddBarViewController.h
+//  Mi9 Bar Finder
+//
+//  Created by Roberto Amici on 4/09/13.
+//  Copyright (c) 2013 NineMSN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Mi9AddBarViewController : UIViewController
+
+@end
---
+++ Mi9 Bar Finder/View Controllers/Mi9AddBarViewController.m
@@ -0,0 +1,38 @@
+//
+//  Mi9AddBarViewController.m
+//  Mi9 Bar Finder
+//
+//  Created by Roberto Amici on 4/09/13.
+//  Copyright (c) 2013 NineMSN. All rights reserved.
+//
+
+#import "Mi9AddBarViewController.h"
+
+@interface Mi9AddBarViewController ()
+
+@end
+
+@implementation Mi9AddBarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
